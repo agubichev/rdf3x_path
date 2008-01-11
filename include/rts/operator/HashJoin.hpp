5353027670b875228b6f2c78bab7a27dea167267
Thomas Neumann <neumann@mpi-inf.mpg.de>
2008-01-11 20:59:06 +0100

    use a dynamic hash table for hash joins

--- include/rts/operator/HashJoin.hpp
+++ include/rts/operator/HashJoin.hpp
@@ -2,6 +2,7 @@
 #define H_rts_operator_HashJoin
 //---------------------------------------------------------------------------
 #include "rts/operator/Operator.hpp"
+#include "infra/util/VarPool.hpp"
 #include <vector>
 //---------------------------------------------------------------------------
 class Register;
@@ -10,17 +11,32 @@ class Register;
 class HashJoin : public Operator
 {
    private:
+   /// A hash table entry
+   struct Entry {
+      /// The next entry
+      Entry* next;
+      /// The key
+      unsigned key;
+      /// The count
+      unsigned count;
+      /// Further values
+      unsigned values[];
+   };
+   /// Helper
+   class Rehasher;
+
    /// The input
    Operator* left,*right;
    /// The join attributes
    Register* leftValue,*rightValue;
    /// The non-join attributes
    std::vector<Register*> leftTail,rightTail;
-
+   /// The pool of hash entry
+   VarPool<Entry> entryPool;
    /// The hash table
-   std::vector<std::vector<unsigned> > hashTable;
+   std::vector<Entry*> hashTable;
    /// The current iter
-   std::vector<unsigned>::const_iterator hashTableIter,hashTableLimit;
+   Entry* hashTableIter;
    /// The tuple count from the right side
    unsigned rightCount;
 
--- rts/operator/HashJoin.cpp
+++ rts/operator/HashJoin.cpp
@@ -2,8 +2,26 @@
 #include "rts/runtime/Runtime.hpp"
 #include <iostream>
 //---------------------------------------------------------------------------
+/// Helper
+class HashJoin::Rehasher {
+   private:
+   /// The hash table
+   std::vector<Entry*>& hashTable;
+
+   public:
+   /// Constructor
+   Rehasher(std::vector<Entry*>& hashTable) : hashTable(hashTable) {}
+
+   /// Rehash
+   void operator()(Entry* g) {
+      Entry*& slot=hashTable[g->key&(hashTable.size()-1)];
+      g->next=slot;
+      slot=g;
+   }
+};
+//---------------------------------------------------------------------------
 HashJoin::HashJoin(Operator* left,Register* leftValue,const std::vector<Register*>& leftTail,Operator* right,Register* rightValue,const std::vector<Register*>& rightTail)
-   : left(left),right(right),leftValue(leftValue),rightValue(rightValue),leftTail(leftTail),rightTail(rightTail)
+   : left(left),right(right),leftValue(leftValue),rightValue(rightValue),leftTail(leftTail),rightTail(rightTail),entryPool(leftTail.size()*sizeof(unsigned))
    // Constructor
 {
 }
@@ -17,30 +35,29 @@ unsigned HashJoin::first()
    // Produce the first tuple
 {
    // Build the hash table from the left side
-   const unsigned hashTableSize = 1503499;
-   const unsigned stepSize = 2+leftTail.size();
+   unsigned hashTableSize = 512,load=0,maxLoad=static_cast<unsigned>(0.8*hashTableSize);
+   unsigned tailLength=leftTail.size();
    hashTable.clear();
    hashTable.resize(hashTableSize);
    for (unsigned leftCount=left->first();leftCount;leftCount=left->next()) {
       // Compute the slot
-      unsigned l=leftValue->value;
-      unsigned slot=l%hashTableSize;
+      unsigned leftKey=leftValue->value;
+      unsigned slot=leftKey&(hashTableSize-1);
 
       // Scan if the entry already exists
-      std::vector<unsigned>& column=hashTable[slot];
       bool match=false;
-      for (unsigned index=0,limit=column.size();index<limit;index+=stepSize)
-         if (l==column[index+1]) {
+      for (Entry* iter=hashTable[slot];iter;iter=iter->next)
+         if (leftKey==iter->key) {
             // Tuple already in the table?
             match=true;
-            for (unsigned index2=0,limit2=stepSize-2;index2<limit2;index++)
-               if (leftTail[index2]->value!=column[index+2+index2]) {
+            for (unsigned index2=0;index2<tailLength;index2++)
+               if (leftTail[index2]->value!=iter->values[index2]) {
                   match=false;
                   break;
                }
             // Then aggregate
             if (match) {
-               column[index]+=leftCount;
+               iter->count+=leftCount;
                break;
             }
          }
@@ -48,10 +65,23 @@ unsigned HashJoin::first()
          continue;
 
       // New tuple, append
-      column.push_back(leftCount);
-      column.push_back(l);
-      for (std::vector<Register*>::const_iterator iter=leftTail.begin(),limit=leftTail.end();iter!=limit;++iter)
-         column.push_back((*iter)->value);
+      Entry* e=entryPool.alloc();
+      e->next=hashTable[slot];
+      hashTable[slot]=e;
+      e->key=leftKey;
+      e->count=leftCount;
+      for (unsigned index2=0;index2<tailLength;index2++)
+         e->values[index2]=leftTail[index2]->value;
+
+      // Rehash?
+      if ((++load)>=maxLoad) {
+         hashTable.clear();
+         hashTableSize*=2;
+         maxLoad=static_cast<unsigned>(0.8*hashTableSize);
+         hashTable.resize(hashTableSize);
+         Rehasher rehasher(hashTable);
+         entryPool.enumAll(rehasher);
+      }
    }
 
    // Read the first tuple from the right side
@@ -59,9 +89,7 @@ unsigned HashJoin::first()
       return false;
 
    // Setup the lookup
-   unsigned slot=rightValue->value%hashTableSize;
-   hashTableIter=hashTable[slot].begin();
-   hashTableLimit=hashTable[slot].end();
+   hashTableIter=hashTable[rightValue->value&(hashTableSize-1)];
 
    return next();
 }
@@ -69,28 +97,24 @@ unsigned HashJoin::first()
 unsigned HashJoin::next()
    // Produce the next tuple
 {
-   const unsigned stepSize = 2+leftTail.size();
-
    // Repeat until a match is found
    while (true) {
       // Still scanning the hash table?
-      while (hashTableIter!=hashTableLimit) {
-         if ((*(hashTableIter+1))==rightValue->value) {
-            unsigned leftCount=*hashTableIter; ++hashTableIter;
-            leftValue->value=*hashTableIter; ++hashTableIter;
-            for (std::vector<Register*>::const_iterator iter=leftTail.begin(),limit=leftTail.end();iter!=limit;++iter,++hashTableIter)
-               (*iter)->value=*hashTableIter;
+      for (;hashTableIter;hashTableIter=hashTableIter->next) {
+         if (hashTableIter->key==rightValue->value) {
+            unsigned leftCount=hashTableIter->count;
+            leftValue->value=hashTableIter->key;
+            for (unsigned index=0,limit=leftTail.size();index<limit;++index)
+               leftTail[index]->value=hashTableIter->values[index];
+            hashTableIter=hashTableIter->next;
             return leftCount*rightCount;
          }
-         hashTableIter+=stepSize;
       }
 
       // Read the next tuple from the right
       if ((rightCount=right->next())==0)
          return false;
-      unsigned slot=rightValue->value%hashTable.size();
-      hashTableIter=hashTable[slot].begin();
-      hashTableLimit=hashTable[slot].end();
+      hashTableIter=hashTable[rightValue->value&(hashTable.size()-1)];
    }
 }
 //---------------------------------------------------------------------------
