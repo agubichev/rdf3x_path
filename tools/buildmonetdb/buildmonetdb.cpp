03d096e7de48ec619ffc58aec26c538a4149a762
Thomas Neumann <neumann@mpi-inf.mpg.de>
2008-03-01 22:48:00 +0100

    omit the reverse strings table
    The reverse table is not required for query execution, only for query
    compilation. As the compilation is manually anway we omit it. The database is
    too small then, though.

--- tools/buildmonetdb/buildmonetdb.cpp
+++ tools/buildmonetdb/buildmonetdb.cpp
@@ -171,9 +171,7 @@ bool readAndStoreStrings(ofstream& out,const char* fileName,const set<unsigned>&
 
    // Prepare the strings table
    // out << "drop table strings;" << endl
-   // out << "drop table reversestrings;" << endl
    out << "create table strings(id int not null primary key, value varchar(4000) not null);" << endl;
-   out << "create table reversestrings(value varchar(4000) not null primary key,id int not null);" << endl;
 
    // Prepare the filter
    static const char* filterStrings[]={"http://simile.mit.edu/2006/01/ontologies/mods3#access","http://simile.mit.edu/2006/01/ontologies/mods3#address","http://simile.mit.edu/2006/01/ontologies/mods3#affiliation","http://simile.mit.edu/2006/01/ontologies/mods3#authority","http://simile.mit.edu/2006/01/ontologies/mods3#catalogingLanguage","http://simile.mit.edu/2006/01/ontologies/mods3#changed","http://simile.mit.edu/2006/01/ontologies/mods3#code","http://simile.mit.edu/2006/01/ontologies/mods3#contents","http://simile.mit.edu/2006/01/ontologies/mods3#copyrightDate","http://simile.mit.edu/2006/01/ontologies/mods3#created","http://simile.mit.edu/2006/01/ontologies/mods3#dateCreated","http://simile.mit.edu/2006/01/ontologies/mods3#dates","http://simile.mit.edu/2006/01/ontologies/mods3#edition","http://simile.mit.edu/2006/01/ontologies/mods3#encoding","http://simile.mit.edu/2006/01/ontologies/mods3#extent","http://simile.mit.edu/2006/01/ontologies/mods3#fullName","http://simile.mit.edu/2006/01/ontologies/mods3#issuance","http://simile.mit.edu/2006/01/ontologies/mods3#language","http://simile.mit.edu/2006/01/ontologies/mods3#nonSort","http://simile.mit.edu/2006/01/ontologies/mods3#origin","http://simile.mit.edu/2006/01/ontologies/mods3#partName","http://simile.mit.edu/2006/01/ontologies/mods3#partNumber","http://simile.mit.edu/2006/01/ontologies/mods3#physicalDescription","http://simile.mit.edu/2006/01/ontologies/mods3#point","http://simile.mit.edu/2006/01/ontologies/mods3#qualifier","http://simile.mit.edu/2006/01/ontologies/mods3#records","http://simile.mit.edu/2006/01/ontologies/mods3#sub","http://www.w3.org/1999/02/22-rdf-syntax-ns#type",0};
@@ -198,9 +196,6 @@ bool readAndStoreStrings(ofstream& out,const char* fileName,const set<unsigned>&
          out << "copy " << stringCache.size() << " records into \"strings\" from stdin using delimiters '\\t';" << endl;
          for (vector<pair<unsigned,string> >::const_iterator iter=stringCache.begin(),limit=stringCache.end();iter!=limit;++iter)
             out << (*iter).first << "\t\"" << escapeCopy((*iter).second) << "\"" << endl;
-         out << "copy " << stringCache.size() << " records into \"reversestrings\" from stdin using delimiters '\\t';" << endl;
-         for (vector<pair<unsigned,string> >::const_iterator iter=stringCache.begin(),limit=stringCache.end();iter!=limit;++iter)
-            out << "\"" << escapeCopy((*iter).second) << "\"\t" << (*iter).first << endl;
          stringCache.clear();
       }
 
@@ -218,9 +213,6 @@ bool readAndStoreStrings(ofstream& out,const char* fileName,const set<unsigned>&
    out << "copy " << stringCache.size() << " records into \"strings\" from stdin using delimiters '\\t';" << endl;
    for (vector<pair<unsigned,string> >::const_iterator iter=stringCache.begin(),limit=stringCache.end();iter!=limit;++iter)
       out << (*iter).first << "\t\"" << escapeCopy((*iter).second) << "\"" << endl;
-   out << "copy " << stringCache.size() << " records into \"reversestrings\" from stdin using delimiters '\\t';" << endl;
-   for (vector<pair<unsigned,string> >::const_iterator iter=stringCache.begin(),limit=stringCache.end();iter!=limit;++iter)
-      out << "\"" << escapeCopy((*iter).second) << "\"\t" << (*iter).first << endl;
 
    // Dump the property names
    // out << "drop table propertynames;" << endl
