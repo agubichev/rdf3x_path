a7a2cdf07cb73919af939262eae037efd4e4bd2b
Thomas Neumann <neumann@mpi-inf.mpg.de>
2009-05-27 13:09:37 +0200

    implement "explain"

--- tools/rdf3xembedded/rdf3xembedded.cpp
+++ tools/rdf3xembedded/rdf3xembedded.cpp
@@ -6,12 +6,15 @@
 #include "cts/semana/SemanticAnalysis.hpp"
 #include "infra/osdep/Timestamp.hpp"
 #include "rts/database/Database.hpp"
+#include "rts/operator/Operator.hpp"
+#include "rts/operator/PlanPrinter.hpp"
+#include "rts/operator/ResultsPrinter.hpp"
 #include "rts/runtime/Runtime.hpp"
 #include "rts/runtime/TemporaryDictionary.hpp"
-#include "rts/operator/Operator.hpp"
-#include "rts/operator/ResultsPrinter.hpp"
+#include "rts/segment/DictionarySegment.hpp"
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <cstdlib>
 //---------------------------------------------------------------------------
 // RDF-3X
@@ -29,6 +32,24 @@ bool smallAddressSpace()
    // Is the address space too small?
 {
    return sizeof(void*)<8;
+}
+//---------------------------------------------------------------------------
+namespace {
+//---------------------------------------------------------------------------
+/// Query types
+enum QueryType { RegularQuery, ExplainQuery, UnknownQueryType };
+//---------------------------------------------------------------------------
+static QueryType classifyQuery(const string& s)
+   // Classify a query
+{
+   SPARQLLexer lexer(s);
+   if (lexer.getNext()!=SPARQLLexer::Identifier)
+      return UnknownQueryType;
+   if (lexer.isKeyword("select"))
+      return RegularQuery;
+   if (lexer.isKeyword("explain"))
+      return ExplainQuery;
+   return UnknownQueryType;
 }
 //---------------------------------------------------------------------------
 static void writeHeader(const QueryGraph& graph,const SPARQLParser& parser)
@@ -108,6 +129,236 @@ static void runQuery(Database& db,const string& query)
    delete operatorTree;
 }
 //---------------------------------------------------------------------------
+/// Output for the explain command
+class ExplainPrinter : public PlanPrinter
+{
+   private:
+   /// The runtime
+   Runtime& runtime;
+   /// The indentation level
+   unsigned indent;
+   /// The operator data
+   string operatorName,operatorArguments;
+   /// Cardinalities
+   double expectedOutputCardinality;
+   /// Cardinalities
+   unsigned observedOutputCardinality;
+   /// Currently in an operator?
+   bool inOp;
+
+   /// Write the current operator if any
+   void flushOperator();
+
+   public:
+   /// Constructor
+   explicit ExplainPrinter(Runtime& runtime);
+   /// Destructor
+   ~ExplainPrinter();
+
+   /// Begin a new operator
+   void beginOperator(const std::string& name,double expectedOutputCardinality,unsigned observedOutputCardinality);
+   /// Add an operator argument annotation
+   void addArgumentAnnotation(const std::string& argument);
+   /// Add a scan annotation
+   void addScanAnnotation(const Register* reg,bool bound);
+   /// Add a predicate annotate
+   void addEqualPredicateAnnotation(const Register* reg1,const Register* reg2);
+   /// Add a materialization annotation
+   void addMaterializationAnnotation(const std::vector<Register*>& regs);
+   /// Add a generic annotation
+   void addGenericAnnotation(const std::string& text);
+   /// Close the current operator
+   void endOperator();
+
+   /// Format a register (for generic annotations)
+   std::string formatRegister(const Register* reg);
+   /// Format a constant value (for generic annotations)
+   std::string formatValue(unsigned value);
+};
+//---------------------------------------------------------------------------
+ExplainPrinter::ExplainPrinter(Runtime& runtime)
+   : runtime(runtime),indent(0),inOp(false)
+   // Constructor
+{
+}
+//---------------------------------------------------------------------------
+ExplainPrinter::~ExplainPrinter()
+   // Destructor
+{
+}
+//---------------------------------------------------------------------------
+template <class T> void escapeOutput(T start,T stop)
+   // Write a string
+{
+   for (T iter=start;iter!=stop;++iter) {
+      char c=*iter;
+      if ((c=='\\')||(c==' ')||(c=='\n')||(c=='\r'))
+         cout << "\\";
+      cout << c;
+   }
+}
+//---------------------------------------------------------------------------
+void ExplainPrinter::flushOperator()
+   // Write the current operator if any
+{
+   if (inOp) {
+      inOp=false;
+      cout << indent << " \"";
+      escapeOutput(operatorName.begin(),operatorName.end());
+      cout << "\" \"";
+      escapeOutput(operatorArguments.begin(),operatorArguments.end());
+      cout << "\" " << expectedOutputCardinality << endl;
+      inOp=false;
+   }
+}
+//---------------------------------------------------------------------------
+void ExplainPrinter::beginOperator(const std::string& name,double expectedOutputCardinality,unsigned observedOutputCardinality)
+   // Begin a new operator
+{
+   flushOperator();
+   operatorName=name;
+   operatorArguments="";
+   this->expectedOutputCardinality=expectedOutputCardinality;
+   this->observedOutputCardinality=observedOutputCardinality;
+   inOp=true;
+   ++indent;
+}
+//---------------------------------------------------------------------------
+void ExplainPrinter::addArgumentAnnotation(const std::string& argument)
+   // Add an operator argument annotation
+{
+   if (operatorArguments.length())
+      operatorArguments+=" ";
+   operatorArguments+=argument;
+}
+//---------------------------------------------------------------------------
+void ExplainPrinter::addScanAnnotation(const Register* reg,bool bound)
+   // Add a scan annotation
+{
+   if (operatorArguments.length())
+      operatorArguments+=" ";
+   if (bound)
+      operatorArguments+=formatValue(reg->value); else
+      operatorArguments+=formatRegister(reg);
+}
+//---------------------------------------------------------------------------
+void ExplainPrinter::addEqualPredicateAnnotation(const Register* reg1,const Register* reg2)
+   // Add a predicate annotate
+{
+   if (operatorArguments.length())
+      operatorArguments+=" ";
+   operatorArguments+=formatRegister(reg1);
+   operatorArguments+="=";
+   operatorArguments+=formatRegister(reg2);
+}
+//---------------------------------------------------------------------------
+void ExplainPrinter::addMaterializationAnnotation(const std::vector<Register*>& /*regs*/)
+   // Add a materialization annotation
+{
+}
+//---------------------------------------------------------------------------
+void ExplainPrinter::addGenericAnnotation(const std::string& /*text*/)
+   // Add a generic annotation
+{
+}
+//---------------------------------------------------------------------------
+void ExplainPrinter::endOperator()
+   // Close the current operator
+{
+   flushOperator();
+   --indent;
+}
+//---------------------------------------------------------------------------
+string ExplainPrinter::formatRegister(const Register* reg)
+   // Format a register (for generic annotations)
+{
+   stringstream result;
+   // Regular register?
+   if (runtime.getRegisterCount()&&(reg>=runtime.getRegister(0))&&(reg<=runtime.getRegister(runtime.getRegisterCount()-1))) {
+      result << "?" << (reg-runtime.getRegister(0));
+   } else {
+      // Arbitrary register outside the runtime system. Should not occur except for debugging!
+      result << "@0x" << hex << reinterpret_cast<uintptr_t>(reg);
+   }
+   return result.str();
+}
+//---------------------------------------------------------------------------
+string ExplainPrinter::formatValue(unsigned value)
+   // Format a constant value (for generic annotations)
+{
+   stringstream result;
+   if (~value) {
+      const char* start,*stop; Type::ID type; unsigned subType;
+      if (runtime.getDatabase().getDictionary().lookupById(value,start,stop,type,subType)) {
+         result << '\"';
+         for (const char* iter=start;iter!=stop;++iter)
+           result << *iter;
+         result << '\"';
+      } else result << "@?" << value;
+   } else {
+      result << "NULL";
+   }
+   return result.str();
+}
+//---------------------------------------------------------------------------
+static void explainQuery(Database& db,const string& query)
+   // Explain a query
+{
+   QueryGraph queryGraph;
+   // Parse the query
+   SPARQLLexer lexer(query);
+   if ((lexer.getNext()!=SPARQLLexer::Identifier)||(!lexer.isKeyword("explain"))) {
+      cout << "internal error: explain expected" << endl;
+      return;
+   }
+   SPARQLParser parser(lexer);
+   try {
+      parser.parse();
+   } catch (const SPARQLParser::ParserException& e) {
+      cout << "parse error: " << e.message << endl;
+      return;
+   }
+
+   // And perform the semantic anaylsis
+   try {
+      SemanticAnalysis semana(db);
+      semana.transform(parser,queryGraph);
+   } catch (const SemanticAnalysis::SemanticException& e) {
+      cout << "semantic error: " << e.message << endl;
+      return;
+   }
+   if (queryGraph.knownEmpty()) {
+      cout << "ok" << endl
+           << "indent operator arguments expectedcardinality" << endl
+           << "1 \"EmptyScan\" \"\" 0" << endl
+           << "\\." << endl;
+      cout.flush();
+      return;
+   }
+
+   // Run the optimizer
+   PlanGen plangen;
+   Plan* plan=plangen.translate(db,queryGraph);
+   if (!plan) {
+      cout << "internal error plan generation failed" << endl;
+      return;
+   }
+
+   // Print the plan
+   cout << "ok" << endl
+        << "indent operator arguments expectedcardinality" << endl;
+   Runtime runtime(db);
+   ExplainPrinter out(runtime);
+   Operator* operatorTree=CodeGen().translate(runtime,queryGraph,plan,false);
+   dynamic_cast<ResultsPrinter*>(operatorTree)->getInput()->print(out);
+   cout << "\\." << endl;
+   cout.flush();
+
+   delete operatorTree;
+}
+//---------------------------------------------------------------------------
+}
+//---------------------------------------------------------------------------
 int main(int argc,char* argv[])
 {
    cout.sync_with_stdio(false);
@@ -138,7 +389,15 @@ int main(int argc,char* argv[])
          }
          query+=c;
       }
-      runQuery(db,query);
+      switch (classifyQuery(query)) {
+         case ExplainQuery:
+            explainQuery(db,query);
+            break;
+         case RegularQuery:
+         default:
+            runQuery(db,query);
+            break;
+      }
       cout.flush();
    }
 }
