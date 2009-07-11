4a7b4447c6ae6963bdec70ddbe01b75bdd694974
Thomas Neumann <neumann@mpi-inf.mpg.de>
2009-07-11 17:30:23 +0200

    add a missing include

--- rts/operator/Selection.cpp
+++ rts/operator/Selection.cpp
@@ -5,6 +5,7 @@
 #include "rts/segment/DictionarySegment.hpp"
 #include <sstream>
 #include <cassert>
+#include <cstdlib>
 #ifdef __GNUC__
 #if (__GNUC__>4)||((__GNUC__==4)&&(__GNUC_MINOR__>=3))
 #define CONFIG_TR1
